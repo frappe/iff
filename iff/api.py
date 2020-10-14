7e217db09f99dd2c50d732747097fda3adf35045
Shivam Mishra <scm.mymail@gmail.com>
2020-10-14 12:25:57 +0530

    feat: added invoice paid webhook

--- iff/api.py
+++ iff/api.py
@@ -29,7 +29,6 @@ def create_member(customer_id, plan, pan=None):
 		"customer_id": customer_id,
 		"subscription_activated": 1,
 		"token_status": "Initiated",
-		"e_mandate": 1,
 		"subscription_start": today,
 		"subscription_end": add_years(today, 2),
 	})
@@ -115,6 +114,71 @@ def token_update():
 
 		frappe.db.set_value("Member", member, 'token_status', token_status)
 		return member
+
+@frappe.whitelist(allow_guest=True)
+def invoice_paid():
+	# https://razorpay.com/docs/api/recurring-payments/webhooks/
+	data = frappe.request.get_data(as_text=True)
+	try:
+		verify_signature(data)
+	except Exception as e:
+		log = frappe.log_error(e, "Webhook Verification Error")
+		return { 'status': 'Failed', 'reason': e}
+
+	if isinstance(data, six.string_types):
+		data = json.loads(data)
+	data = frappe._dict(data)
+
+	payment = frappe._dict(data.payload.get("payment", {}).get("entity", {}))
+
+	if not payment.method == "emandate":
+		return
+
+	controller = frappe.get_doc("Razorpay Settings")
+	controller.init_client()
+	client = controller.client
+
+	today = getdate()
+
+	member = frappe.db.exists('Member', {'customer_id': payment.customer_id})
+	token_data = client.token.fetch(payment.customer_id, payment.token_id)
+
+	if not member:
+		max_amount = token_data.get('max_amount') / 100
+		plan = frappe.db.exists('Membership Type', { 'amount':  max_amount})
+		if plan:
+			plan_id = frappe.db.get_value("Membership Type", plan, "razorpay_plan_id")
+			member = create_member(payment.customer_id, plan_id)
+
+	if member:
+		member = frappe.get_doc("Member", member)
+
+		membership = frappe.new_doc("Membership")
+		membership.update({
+			"member": member.name,
+			"membership_status": "New",
+			"membership_type": member.membership_type,
+			"currency": "INR",
+			"paid": 1,
+			"payment_id": payment.id,
+			"from_date": today,
+			"to_date": add_months(today, 1),
+			"amount": frappe.db.get_value("Membership Type", member.membership_type, "amount")
+		})
+		membership.insert(ignore_permissions=True)
+
+		# Update membership values
+		member.subscription_activated = 1
+		member.e_mandate = 1
+		member.razorpay_token = payment.token_id
+		status = token_data.get('recurring_details').get('status')
+		if status == "confirmed":
+			member.token_status = "Confirmed"
+		if status == "rejected":
+			member.token_status = "Rejected"
+
+		member.membership_expiry_date = add_months(today, 1)
+		member.save(ignore_permissions=True)
 
 @frappe.whitelist(allow_guest=True)
 def ping():
